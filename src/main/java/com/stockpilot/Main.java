f48e810f55d38d39435000a6e1e0aef49f87328f
GeorgeTymk <georgekumwenda0@gmail.com>
2026-07-24 22:16:31 +0200

    Working version before installer rebuild

--- src/main/java/com/stockpilot/Main.java
+++ src/main/java/com/stockpilot/Main.java
@@ -1,35 +1,10 @@
 package com.stockpilot;
 
-
-import com.stockpilot.database.Database;
-import com.stockpilot.database.DatabaseInitializer;
-import com.stockpilot.database.UserSeeder;
-
-import java.sql.Connection;
-
-
-
 public class Main {
 
-
-    public static void main(String[] args) throws Exception {
-
-
-        try(Connection connection = Database.connect()){
-
-
-            DatabaseInitializer.createTables();
-
-
-            UserSeeder.createAdmin();
-
-
-        }
-
-
+    public static void main(String[] args) {
 
         App.main(args);
-
 
     }
 
